d2d2367d7ff9634a9f1ec6f49544a1c1b37f4782
Leonardo Santos <leonardojosantos@gmail.com>
2023-12-27 16:37:23 -0300

    Project created

---
+++ course/src/main/java/com/educandoweb/course/CourseApplication.java
@@ -0,0 +1,16 @@
+/*-------------------- packages --------------------*/
+package com.educandoweb.course;
+
+/*-------------------- libraries --------------------*/
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*-------------------- class CourseApplication --------------------*/
+@SpringBootApplication
+public class CourseApplication {
+
+	/*-------------------- main method --------------------*/
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+}
