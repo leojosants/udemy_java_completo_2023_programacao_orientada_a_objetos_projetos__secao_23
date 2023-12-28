650f198d67442af0b2c9c408b025ce99842afb30
Leonardo Santos <leonardojosantos@gmail.com>
2023-12-27 23:10:25 -0300

    OrderItem, many-to-many association with extra attributes - part 1

---
+++ course/src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -0,0 +1,91 @@
+/*-------------------- packages --------------------*/
+package com.educandoweb.course.entities;
+
+/*-------------------- imports --------------------*/
+import java.io.Serializable;
+import com.educandoweb.course.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/*-------------------- class OrderItem --------------------*/
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+
+	/*-------------------- attributes --------------------*/
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private OrderItemPK id;
+
+	private Integer quantity;
+	private Double price;
+	
+	/*-------------------- constructors --------------------*/
+	public OrderItem() {}
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+		this.id.setOrder(order);
+		this.id.setProduct(product);
+		this.setQuantity(quantity);
+		this.setPrice(price);
+	}
+	
+	/*-------------------- attributes --------------------*/
+	public Order getOrder() {
+		return this.id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		this.id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return this.id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		this.id.setProduct(product);
+	}
+	
+	public Integer getQuantity() {
+		return this.quantity;
+	}
+	
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+	public Double getPrice() {
+		return this.price;
+	}
+	
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	/*-------------------- methods --------------------*/
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+
+		OrderItem other = (OrderItem) obj;
+		
+		if (this.id == null) {
+			if (other.id != null) return false;
+		} 
+		else if (!this.id.equals(other.id)) return false;
+
+		return true;
+	}
+}
---
+++ course/src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,74 @@
+/*-------------------- packages --------------------*/
+package com.educandoweb.course.entities.pk;
+
+/*-------------------- imports --------------------*/
+import java.io.Serializable;
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+/*-------------------- class OrderItemPK --------------------*/
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+	/*-------------------- attributes --------------------*/
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+
+	/*-------------------- getters and setters --------------------*/
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	/*-------------------- methods --------------------*/
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.getOrder() == null) ? 0 : this.getOrder().hashCode());
+		result = prime * result + ((this.getProduct() == null) ? 0 : this.getProduct().hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+
+		OrderItemPK other = (OrderItemPK) obj;
+		
+		if (this.getOrder() == null) {
+			if (other.getOrder()  != null) return false;
+		} 
+		else if (!this.getOrder().equals(other.getOrder() )) return false;
+
+		if (this.getProduct() == null) {
+			if (other.getProduct() != null) return false;
+		} 
+		else if (!this.getProduct().equals(other.getProduct())) return false;
+		
+		return true;
+	}
+}
